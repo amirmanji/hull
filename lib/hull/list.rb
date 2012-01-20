68c09eff2a348d159051c1ecd4d9529b18a58f71
hc5duke <hc5duke@gmail.com>
2012-01-20 02:28:56 -0800

    List refactor
    should probably write some specs...

--- lib/hull/list.rb
+++ lib/hull/list.rb
@@ -15,39 +15,43 @@ module Hull
       branch_len = [ branch_names.map(&:last).map(&:length).max,  30 ].min
 
       pulls.each do |pull|
-        number      = pull['number'].to_s
-        mergeable   = pull['mergeable']
-        title       = pull['title']
-        created_at  = Time.parse(pull['issue_created_at'])
-        repo_name, branch_name = pull['head']['label'].split(/:/)
-
-        if repo_name == username
-          color = :yellow
-        else
-          if mergeable
-            is_old = repo.branches.map(&:name).any? { |branch| branch =~ /^. *cr\/#{number}/}
-            color = is_old ? :purple : :white
-          else
-            color = :red
-          end
-        end
-
-        number      = pad_left(number, 6)
-        title       = pad_right(title, column_size - repo_len - branch_len - 10)
-        repo_name   = pad_left(repo_name, repo_len)
-        branch_name = pad_right(branch_name, branch_len)
-
-        puts [
-          write(number, color),
-          [
-            write(repo_name,   :yellow),
-            branch_name.split(/\//).map do |branch|
-              write(branch, :green)
-            end.join('/'),
-          ].join('/'),
-          title,
-        ].join(' ')
+        puts pull_info(pull)
       end
+    end
+
+    def pull_info(pull)
+      number      = pull['number'].to_s
+      mergeable   = pull['mergeable']
+      title       = pull['title']
+      created_at  = Time.parse(pull['issue_created_at'])
+      repo_name, branch_name = pull['head']['label'].split(/:/)
+
+      if repo_name == username
+        color = :yellow
+      else
+        if mergeable
+          is_old = repo.branches.map(&:name).any? { |branch| branch =~ /^. *cr\/#{number}/}
+          color = is_old ? :purple : :white
+        else
+          color = :red
+        end
+      end
+
+      number      = pad_left(number, 6)
+      title       = pad_right(title, column_size - repo_len - branch_len - 10)
+      repo_name   = pad_left(repo_name, repo_len)
+      branch_name = pad_right(branch_name, branch_len)
+
+      [
+        write(number, color),
+        [
+          write(repo_name,   :yellow),
+          branch_name.split(/\//).map do |branch|
+            write(branch, :green)
+          end.join('/'),
+        ].join('/'),
+        title,
+      ].join(' ')
     end
   end
 end
