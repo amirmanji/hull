94fb409cdf1cc22d47dcf3d3923145ba6923d4ea
hc5duke <hc5duke@gmail.com>
2012-01-19 20:16:46 -0800

    Simple parsing of arguments

--- lib/puller.rb
+++ lib/puller.rb
@@ -7,8 +7,9 @@ class Puller
 
   def initialize(*argv)
     parse_args(argv.clone)
-    puts [command, number]
   end
+
+  private
 
   def parse_args(args)
     self.command = args.shift.strip rescue 'help'
@@ -16,8 +17,17 @@ class Puller
     when 'list'
     when *%w(show get rm)
       self.number  = args.shift.strip rescue ''
+      if valid_number?
+        self.number = number.to_i
+      else
+        self.command = 'help'
+      end
     else
       self.command = 'help'
     end
+  end
+
+  def valid_number?
+    number.to_s.match(/\d+/)
   end
 end
--- spec/puller_spec.rb
+++ spec/puller_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 
 describe Puller do
   describe "Parsing arguments" do
-    it "should handle empty args" do
-      args = ""
-      puller = Puller.new(args)
-      puller.command.should == 'help'
-      puller.number.should be_nil
+    it "should handle bad args" do
+      commands = [ '', 'asdf', 'foo' ]
+      commands.each do |command|
+        puller = Puller.new(command)
+        puller.command.should == 'help'
+        puller.number.should be_nil
+      end
+    end
+
+    it "should handle good args" do
+      commands = %w(list show get rm)
+      commands.each do |command|
+        args = "#{command} 10"
+        puller = Puller.new(args)
+        puller.command.should == 'help'
+        puller.number.should be_nil
+      end
     end
   end
 end
